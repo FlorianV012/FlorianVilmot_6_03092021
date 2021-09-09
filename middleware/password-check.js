51070ce17f008a3fc6e403c1eac7ba535fba77e0
FlorianV012 <fvilmot@gmail.com>
2021-09-09 14:40:56 +0200

    Implémentation du module password-validator et création de middlewares pour vérifier les emails et mdp

---
+++ middleware/password-check.js
@@ -0,0 +1,9 @@
+const passwordSchema = require('../models/password');
+
+module.exports = (req, res, next) => {
+    if (!passwordSchema.validate(req.body.password)) {
+        res.status(400).json({ message: 'Le mot de passe doit avoir une longueur minimale de 10 caractères et comporter au moins une majuscule, une minuscule et un chiffre.' });
+    } else {
+        next();
+    }
+};
