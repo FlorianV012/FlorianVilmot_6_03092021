b678d78307db1c841a3f7bc5d35f2aed0444cab1
FlorianV012 <fvilmot@gmail.com>
2021-09-13 09:30:00 +0200

    Correction du contrôleur getOneSauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -10,7 +10,7 @@ exports.getAllSauce = (req, res, next) => {
 
 // Affiche la sauce sélectionnée
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne()
+    Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(400).json({ error }));
 };
